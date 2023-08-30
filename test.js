8a7fae3080ac6c01639dc880543c2dba6e303886
Junbeom <127077119+junbeom-imgibble@users.noreply.github.com>
2023-08-30 15:08:13 +0900

    find section

--- test.js
+++ test.js
@@ -1,4 +1,1 @@
-document.body.insertAdjacentHTML("afterbegin",
-    `<section class="section" data-ez-module="html/1" data-ez-name="CUSTOM HTML" data-ez-type="plain" data-ez="contents-185rhsi-1">
-<shorts-works></shorts-works>
-</section>`)
+console.log(document.querySelectorAll("section"))
