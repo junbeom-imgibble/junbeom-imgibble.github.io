4dc94c3f41a7b6d0cd31f880b1e757e74596582c
Junbeom <127077119+junbeom-imgibble@users.noreply.github.com>
2023-08-30 15:17:31 +0900

    dynamic

---
+++ dynamic.js
@@ -0,0 +1,4 @@
+document.body.insertAdjacentHTML("afterbegin",
+    `<section class="section" data-ez-module="html/1" data-ez-name="CUSTOM HTML" data-ez-type="plain" data-ez="contents-185rhsi-1">
+<shorts-works></shorts-works>
+</section>`)
